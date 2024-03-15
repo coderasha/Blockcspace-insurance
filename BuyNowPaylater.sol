b31e5ea1b27c6d0f9f4b37375d9b4c61f124c53f
Ashadipta Pati <44902407+coderasha@users.noreply.github.com>
2024-03-15 16:18:20 +0530

    Update BuyNowPaylater.sol
    BuyNowPaylater contract declaration and variables

--- BuyNowPaylater.sol
+++ BuyNowPaylater.sol
@@ -4,3 +4,11 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+contract Buynowpaylater {
+    using SafeMath for uint256;
+
+    IERC20 private ashaToken;
+    address  payable private insuranceCompany;
+     uint256 public asha_token_Supply = 10000*10**18; // Total supply of asha-token
+    uint256 public each_asha_token_Value = 0.0001*10**18 wei; // Value of each asha-token in ether
+    uint256 public asha_token_Value = 1*10**18 wei; // Value of the property in ether
