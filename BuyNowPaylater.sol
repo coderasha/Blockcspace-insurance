4749b52ffd9173f137b55c0219039981b6999209
Ashadipta Pati <44902407+coderasha@users.noreply.github.com>
2024-03-15 16:19:57 +0530

    Update BuyNowPaylater.sol
    User struct

--- BuyNowPaylater.sol
+++ BuyNowPaylater.sol
@@ -12,3 +12,9 @@ contract Buynowpaylater {
      uint256 public asha_token_Supply = 10000*10**18; // Total supply of asha-token
     uint256 public each_asha_token_Value = 0.0001*10**18 wei; // Value of each asha-token in ether
     uint256 public asha_token_Value = 1*10**18 wei; // Value of the property in ether
+  struct User {
+        uint256 ashaTokenBalance;
+        uint256 owedToInsuranceCompany;
+        uint256 installmentDueDate;
+    }
+
