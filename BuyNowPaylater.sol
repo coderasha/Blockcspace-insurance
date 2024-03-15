4f20557564af7673050181bc6d2016c6b0ae305b
Ashadipta Pati <44902407+coderasha@users.noreply.github.com>
2024-03-15 16:23:58 +0530

    Update BuyNowPaylater.sol
    full update

--- BuyNowPaylater.sol
+++ BuyNowPaylater.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
 contract Buynowpaylater {
     using SafeMath for uint256;
 
@@ -12,24 +13,28 @@ contract Buynowpaylater {
      uint256 public asha_token_Supply = 10000*10**18; // Total supply of asha-token
     uint256 public each_asha_token_Value = 0.0001*10**18 wei; // Value of each asha-token in ether
     uint256 public asha_token_Value = 1*10**18 wei; // Value of the property in ether
-  struct User {
+  
+
+
+    struct User {
         uint256 ashaTokenBalance;
         uint256 owedToInsuranceCompany;
         uint256 installmentDueDate;
     }
-mapping(address => User) private users;
+
+    mapping(address => User) private users;
 mapping(address =>uint256) public balances;
     event Bought(address indexed buyer, uint256 ashaTokenAmount);
     event Paid(address indexed buyer, uint256 amount);
     event Received(address indexed buyer, uint256 amount);
      event FundsTransferred(address indexed sender, uint256 amount);
 
- constructor(IERC20 _ashaToken, address payable _insuranceCompany) {
+    constructor(IERC20 _ashaToken, address payable _insuranceCompany) {
         ashaToken = _ashaToken;
        insuranceCompany = _insuranceCompany;
     }
 
-function buy(uint256 percentage) external payable {
+    function buy(uint256 percentage) external payable {
         require(percentage >= 10 && percentage <= 50, "Invalid percentage");
         uint256 ashaTokenAmount = (msg.value).mul(10000);
         // require(msg.value >= ashaTokenAmount, "Insufficient ETH");
@@ -52,7 +57,8 @@ function buy(uint256 percentage) external payable {
         // emit FundsTransferred(insuranceCompany, 1 ether);
        
     }
-function pay() external payable{ 
+
+    function pay() external payable{ 
         //require(users[msg.sender].owedToInsuranceCompany >= amount, "Insufficient amount to pay");
         //require(block.timestamp >= users[msg.sender].installmentDueDate, "Installment not due yet");
         //users[msg.sender].owedToInsuranceCompany = users[msg.sender].owedToInsuranceCompany - msg.value*10000;
@@ -63,17 +69,17 @@ function pay() external payable{
         users[msg.sender].owedToInsuranceCompany -= asha_token_to_transfer_by_insur;
         users[msg.sender].ashaTokenBalance += asha_token_to_transfer_by_insur;
         // balances[insuranceCompany] -= msg.value.mul(10000);
-
-function insuranceInfo(address user) external view returns (uint256) {
-        return users[user].owedToInsuranceCompany;
-    }
         // balances[msg.sender] += msg.value.mul(10000);
         emit Paid(msg.sender, msg.value);
     }
- function getUserInfo(address user) external view returns (uint256, uint256, uint256) {
+    function insuranceInfo(address user) external view returns (uint256) {
+        return users[user].owedToInsuranceCompany;
+    }
+
+    function getUserInfo(address user) external view returns (uint256, uint256, uint256) {
         return (users[user].ashaTokenBalance, users[user].owedToInsuranceCompany, users[user].installmentDueDate);
     }
- function getInsuranceEthBalance() external view returns (uint256) {
+    function getInsuranceEthBalance() external view returns (uint256) {
         return insuranceCompany.balance;
     }
      function getcontractBalance() external view returns (uint256) {
