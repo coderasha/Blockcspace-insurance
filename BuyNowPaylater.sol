78800faf95b68c1877603ffa2f52c87607a6feb7
Ashadipta Pati <44902407+coderasha@users.noreply.github.com>
2024-03-15 16:17:35 +0530

    Update BuyNowPaylater.sol
    License and imports

--- BuyNowPaylater.sol
+++ BuyNowPaylater.sol
@@ -1,1 +1,6 @@
 
+///SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
